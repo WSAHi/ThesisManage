8782b77305950ee2591b0e3ff8e5125b4100b449
王顺安 <wsa_hi@outlook.com>
2016-02-07 14:09:18 +0800

    修改代码

--- ThesisManage/Admin/AdminMasterPage.master.cs
+++ ThesisManage/Admin/AdminMasterPage.master.cs
@@ -18,15 +18,15 @@ public partial class Admin_AdminMasterPage : System.Web.UI.MasterPage
     ThesisManage.BLL.ThesisManage thesisManage = new ThesisManage.BLL.ThesisManage();
     protected void Page_Load(object sender, EventArgs e)
     {
-        int studentTotalNum = studentManage.GetStudentCount();
-        int titleNum = titleManage.GetTitleCount();
+        int studentCounts = studentManage.GetStudentCount();
+        int titleCounts = titleManage.GetTitleCount();
         int titleCountSum = titleManage.GetTitleCountsSum();
-        int hasChooseTitleStuNum = studentManage.GetHasChooseTitleNum();
-        int upLoadThesisStuNum = thesisManage.GetThesisCount();
-        lblStudentTotaloty.Text = studentTotalNum.ToString();
-        lblTitleTotality.Text = titleNum.ToString();
+        int hasChooseTitleNum = studentManage.GetHasChooseTitleNum();
+        int upLoadThesisNum = thesisManage.GetThesisCount();
+        lblStudentTotaloty.Text = studentCounts.ToString();
+        lblTitleTotality.Text = titleCounts.ToString();
         lblTitltCanChooseTotality.Text = titleCountSum.ToString();
-        lblHasChooseTitleStudentNum.Text = hasChooseTitleStuNum.ToString();
-        lblHasUploadThesisNum.Text = upLoadThesisStuNum.ToString();
+        lblHasChooseTitleStudentNum.Text = hasChooseTitleNum.ToString();
+        lblHasUploadThesisNum.Text = upLoadThesisNum.ToString();
     }
 }
