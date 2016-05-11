51ed3b9fad9d3d22de0cda61dc3a0257417908f8
王顺安 <wsa_hi@outlook.com>
2016-05-11 16:42:29 +0800

    修改变量名

--- ThesisManage/Student/ChooseTitle.aspx.cs
+++ ThesisManage/Student/ChooseTitle.aspx.cs
@@ -30,32 +30,27 @@ public partial class Student_ChooseTitle : System.Web.UI.Page
     {
         if (e.Row.RowType == DataControlRowType.DataRow)
         {
-            //设置行颜色   
-            e.Row.Attributes.Add("onmouseover", "currentcolor=this.style.backgroundColor;this.style.backgroundColor='#ff9900'");
-            //添加自定义属性，当鼠标移走时还原该行的背景色   
-            e.Row.Attributes.Add("onmouseout", "this.style.backgroundColor=currentcolor");
-            //添加删除确认
-            Button btn = (Button)e.Row.FindControl("btnChooseTitle");
-            btn.Attributes.Add("onclick", "return confirm('您确认要选择该论题吗?');");
+            Button btnChooseTitle = (Button)e.Row.FindControl("btnChooseTitle");
+            btnChooseTitle.Attributes.Add("onclick", "return confirm('您确认要选择该论题吗?');");
         }
     }
     protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
     {
-        String studentId = ((Student)Session["student"]).StudentID;
+        String studentID = ((Student)Session["student"]).StudentID;
         int studentState = ((Student)Session["student"]).SudentState;
         if (studentState == 0)
         {
-            int TitleId = Convert.ToInt32(e.CommandArgument);
-            Title title = titleManage.GetTilteByTitleID(TitleId);
+            int TitleID = Convert.ToInt32(e.CommandArgument);
+            Title title = titleManage.GetTilteByTitleID(TitleID);
             if (title.HasChooseNum < title.Counts)
             {
-                int num = studentManage.ModifiyStuSate(1, TitleId, studentId);
+                int num = studentManage.ModifiyStuSate(1, TitleID, studentID);
                 if (num > 0)
                 {
-                    int num1 = titleManage.ModifiyTitleHasChooseNum(TitleId);
-                    if (num1 > 0)
+                    int ModifyTitleHasChooseNum = titleManage.ModifiyTitleHasChooseNum(TitleID);
+                    if (ModifyTitleHasChooseNum > 0)
                     {
-                        Student student = studentManage.GetStudentByStudentID(studentId);
+                        Student student = studentManage.GetStudentByStudentID(studentID);
                         Session.Remove("student");
                         Session["student"] = student;
                         this.Page.ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('选题成功！');window.location.href='StudentMessage.aspx';</script>");
