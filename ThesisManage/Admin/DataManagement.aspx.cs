dfc60e4e8290b92bdbae540d85c3c3dd47102f87
王顺安 <wsa_hi@outlook.com>
2016-05-11 23:53:04 +0800

    删除多余空行

--- ThesisManage/Admin/DataManagement.aspx.cs
+++ ThesisManage/Admin/DataManagement.aspx.cs
@@ -37,7 +37,6 @@ public partial class Admin_DataManagement : System.Web.UI.Page
         }
         string UserRole = ddlUserRole.SelectedItem.Text.Trim();
         btnAddLost.Attributes.Add("onclick", "return confirm('您是要添加【" + UserRole + "】表的数据吗？');");
-
     }
     protected void ddlUserRole_SelectedIndexChanged(object sender, EventArgs e)
     {
